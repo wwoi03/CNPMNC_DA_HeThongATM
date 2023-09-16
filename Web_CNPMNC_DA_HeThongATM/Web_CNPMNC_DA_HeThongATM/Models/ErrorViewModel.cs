e695844b6e2ec3085181e63b67b393d4e0d5c14b
wwoi03 <tuandao8826848@gmail.com>
2023-09-16 21:22:03 +0700

    start

---
+++ Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_CNPMNC_DA_HeThongATM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
