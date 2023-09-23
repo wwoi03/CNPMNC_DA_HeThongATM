94eb18f1abdbd229b9bb4130b54a0ff72f98b7a9
wwoi03 <tuandao8826848@gmail.com>
2023-09-23 12:22:57 +0700

    cập nhật lại connect firebase

---
+++ App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/Activities/LoginActivity.java
@@ -0,0 +1,188 @@
+package com.example.app_cnpmnc_da_hethongatm.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.app_cnpmnc_da_hethongatm.MainActivity;
+import com.example.app_cnpmnc_da_hethongatm.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText phone , Otp, edtPass;
+    Button btnLogin;
+    TextView btnReset;
+    FirebaseAuth mAuth;
+    String vertifycationID;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference useRef;
+
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        phone= findViewById(R.id.edtPhone);
+        edtPass=findViewById(R.id.edtPass);
+        btnLogin=findViewById(R.id.btnLogin);
+        mAuth=FirebaseAuth.getInstance();
+        btnReset=findViewById(R.id.btnReset);
+
+        firebaseDatabase = FirebaseDatabase.getInstance("https://systematm-aea2c-default-rtdb.asia-southeast1.firebasedatabase.app");
+        useRef = firebaseDatabase.getReference("User");
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(phone.getText().toString()))
+                {
+                    Toast.makeText(LoginActivity.this," Enter valid Phone No.",Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(edtPass.getText().toString())){
+                    Toast.makeText(LoginActivity.this,"Nhap password",Toast.LENGTH_SHORT).show();
+                } else if (count==5) {
+                    Toast.makeText(LoginActivity.this,"GG",Toast.LENGTH_SHORT).show();
+                } else
+                {
+                    useRef.child(phone.getText().toString()).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String pass = snapshot.child("password").getValue().toString();
+                            if (pass.equals(edtPass.getText().toString())){
+                                String number = phone.getText().toString();
+                                Sendverificationcode(number);
+
+                                Toast.makeText(LoginActivity.this, "Mk dung", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                count++;
+                                Toast.makeText(LoginActivity.this, "Mk sai", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgetPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void verificationcode()
+    {
+        //PhoneAuthCredential credential = PhoneAuthProvider.getCredential(vertifycationID,Code);
+        //signinbyCredentials(credential);
+        Intent intent = new Intent(LoginActivity.this, OTPVerificationActivity.class);
+        intent.putExtra("vertifycationID", vertifycationID);
+        intent.putExtra("phoneNumber", phone.getText().toString());
+        startActivity(intent);
+    }
+
+    private void signinbyCredentials(PhoneAuthCredential credential)
+    {
+        FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(LoginActivity.this, "Login Successfull", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        }
+                    }
+                });
+    }
+
+    private void Sendverificationcode(String phoneNumber)
+    {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber("+84" + phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // (optional) Activity for callback binding
+                        // If no activity is passed, reCAPTCHA verification can not be used.
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted( @NonNull PhoneAuthCredential credential)
+        {
+           /* final String code = credential.getSmsCode();
+            if(code!=null)
+            {
+                verificationcode(code);
+                Toast.makeText(LoginActivity.this," Verifycation ok",Toast.LENGTH_SHORT).show();
+
+            }*/
+            verificationcode();
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+            Toast.makeText(LoginActivity.this," Verifycation Failed",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String s,
+                               @NonNull PhoneAuthProvider.ForceResendingToken token) {
+            super.onCodeSent(s,token);
+            vertifycationID = s;
+            verificationcode();
+        }
+    };
+    @Override
+    protected void onStart(){
+        super.onStart();
+//         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+//         if(currentUser!=null)
+//         {
+//            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+//            finish();
+//         }
+    }
+
+}
