5506edca6d8377b2a5ab0a1d8e4af3e3db3c6e00
Trần Phương Nam <namtran0108642@gmail.com>
2023-09-23 13:06:09 +0700

    sửa và thêm giao diện

---
+++ App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/ManageActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_cnpmnc_da_hethongatm;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ManageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rowthuhuong);
+    }
+}
