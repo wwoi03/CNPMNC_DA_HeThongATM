5d6db3670f34d21f3487b36a62a96f73bedaeeb7
wwoi03 <tuandao8826848@gmail.com>
2023-09-26 00:36:34 +0700

    new

--- App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/MainActivity.java
+++ App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/MainActivity.java
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     TransactionFragment transactionFragment;
     QuickAccessFragment quickAccessFragment;
 
+    // Code moiws
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
