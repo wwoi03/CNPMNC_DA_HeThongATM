b9ad56c35db5d0c0035dae15e25a06fab4b055a2
Gezellighied <diepminhquan123@gmail.com>
2023-09-26 20:32:51 +0700

    hiển thị danh sách khách hàng

---
+++ Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Controllers/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_CNPMNC_DA_HeThongATM.Models;
+
+namespace Web_CNPMNC_DA_HeThongATM.Controllers
+{
+    public class Customer : Controller
+    {
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
+        public async Task<IActionResult> Index()
+        {
+            ViewData["j"] = await firebaseHelper.GetCustommers();
+            return View();
+        }
+    }
+}
