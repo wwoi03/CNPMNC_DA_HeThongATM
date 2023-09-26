41b633788201f36b8d95155c10caf4ed7949b0e0
An8bit <ttkit35@gmail.com>
2023-09-26 15:54:39 +0700

    update

--- Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Models/FirebaseHelper.cs
+++ Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Models/FirebaseHelper.cs
@@ -15,7 +15,8 @@ namespace Web_CNPMNC_DA_HeThongATM.Models
             AuthSecret = "086JcgQrRLjvg3lubA1YY9GlAvks4VrYTCeWJJy6",
             BasePath = "https://systematm-aea2c-default-rtdb.asia-southeast1.firebasedatabase.app/"
         };
-        public FirebaseHelper() {
+        public FirebaseHelper()
+        {
             client = new FirebaseClient(config);
         }
         public static async Task WriteLog(string name, string Ds)
@@ -27,103 +28,105 @@ namespace Web_CNPMNC_DA_HeThongATM.Models
             };
             FirebaseResponse response = await client.PushAsync("LogDebug", logSystem);
         }
-            public async Task<List<CustommerViewModel>> GetCustommers()
+        public async Task<List<CustommerViewModel>> GetCustommers()
+        {
+            try
             {
-                try
+                FirebaseResponse response = await client.GetAsync("KhachHang");
+                if (response != null)
                 {
-                    FirebaseResponse response = await client.GetAsync("KhachHang");
-                    if (response != null)
-                    {
-                        Dictionary<string, CustommerViewModel> data = JsonConvert.DeserializeObject<Dictionary<string, CustommerViewModel>>(response.Body);
-                        List<CustommerViewModel> peopleList = new List<CustommerViewModel>(data.Values);
-                        return peopleList;
-                    }
-                    else
-                    {
-                        Console.WriteLine(response.StatusCode);
-                        return null;
-                    }
+                    Dictionary<string, CustommerViewModel> data = JsonConvert.DeserializeObject<Dictionary<string, CustommerViewModel>>(response.Body);
+                    List<CustommerViewModel> peopleList = new List<CustommerViewModel>(data.Values);
+                    return peopleList;
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(response.StatusCode);
                     return null;
                 }
             }
-            public async Task InsertCustommer(CustommerViewModel custommer)
+            catch (Exception ex)
             {
-                try
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public async Task InsertCustommer(CustommerViewModel custommer)
+        {
+            try
+            {
+                FirebaseResponse response = await client.PushAsync("KhachHang", custommer);
+                if (response != null)
                 {
-                    FirebaseResponse response = await client.PushAsync("KhachHang", custommer);
-                    if (response != null)
-                    {
-
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
 
                 }
             }
-            public async Task<string> CreateidCus(string idcus)
+            catch (Exception ex)
             {
-                try
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+        public async Task<string> CreateidCus(string idcus)
+        {
+            try
+            {
+
+                FirebaseResponse response = await client.GetAsync("KhachHang");
+
+                if (response == null || response.Body == "null")
                 {
-
-                    FirebaseResponse response = await client.GetAsync("KhachHang");
-
-                    if (response == null || response.Body == "null")
-                    {
-                        return ""; // Node không tồn tại hoặc trống
-                        await WriteLog("CreateidCus", "data null");
-                    }
-
-                    Dictionary<string, CustommerViewModel> data = response.ResultAs<Dictionary<string, CustommerViewModel>>();
-
-
-                    return; // CCCD không tồn tại trong danh sách
+                    return ""; // Node không tồn tại hoặc trống
+                    await WriteLog("CreateidCus", "data null");
                 }
-                catch (Exception ex)
-                {
-                    // Xử lý lỗi nếu có
-                    Console.WriteLine(ex.Message);
 
-                }
+                Dictionary<string, CustommerViewModel> data = JsonConvert.DeserializeObject<Dictionary<string, CustommerViewModel>>(response.Body);
+                List<CustommerViewModel> peopleList = new List<CustommerViewModel>(data.Values);
+
+                return "CTM194500"+(peopleList.Count + 1).ToString(); 
+
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi nếu có
+                Console.WriteLine(ex.Message);
 
             }
 
-            public async Task<bool> CheckCCCDExist(string cccdToCheck)
+        }
+
+        public async Task<bool> CheckCCCDExist(string cccdToCheck)
+        {
+            try
             {
-                try
+
+                FirebaseResponse response = await client.GetAsync("KhachHang");
+
+                if (response == null || response.Body == "null")
                 {
-
-                    FirebaseResponse response = await client.GetAsync("KhachHang");
-
-                    if (response == null || response.Body == "null")
-                    {
-                        return false; // Node không tồn tại hoặc trống
-                    }
-
-                    Dictionary<string, CustommerViewModel> data = response.ResultAs<Dictionary<string, CustommerViewModel>>();
-
-                    if (data != null && data.Values.Any(item => item.CCCD == cccdToCheck))//Any() để kiểm tra xem có bất kỳ phần tử nào trong danh sách có trường CCCD bằng với giá trị cccdToCheck hay không.
-                    {
-                        return true; // CCCD tồn tại trong danh sách
-                    }
-
-                    return false; // CCCD không tồn tại trong danh sách
+                    return false; // Node không tồn tại hoặc trống
                 }
-                catch (Exception ex)
+
+                Dictionary<string, CustommerViewModel> data = response.ResultAs<Dictionary<string, CustommerViewModel>>();
+
+                if (data != null && data.Values.Any(item => item.CCCD == cccdToCheck))//Any() để kiểm tra xem có bất kỳ phần tử nào trong danh sách có trường CCCD bằng với giá trị cccdToCheck hay không.
                 {
-                    // Xử lý lỗi nếu có
-                    Console.WriteLine(ex.Message);
-                    return false;
+                    return true; // CCCD tồn tại trong danh sách
                 }
+
+                return false; // CCCD không tồn tại trong danh sách
             }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi nếu có
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
 
 
 
-        } } 
-   
+        
+    }
+}
 
