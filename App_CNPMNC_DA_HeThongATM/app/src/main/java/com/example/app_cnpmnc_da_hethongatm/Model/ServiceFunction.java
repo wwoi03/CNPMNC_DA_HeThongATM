8bfe9c399d6ff3fb524fef8ec72e3148e5cff7f4
wwoi03 <tuandao8826848@gmail.com>
2023-09-30 21:09:45 +0700

    cập nhật model ServiceFunction

---
+++ App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/Model/ServiceFunction.java
@@ -0,0 +1,15 @@
+package com.example.app_cnpmnc_da_hethongatm.Model;
+
+public class ServiceFunction {
+    private String MaChucNang;
+    private String TenChucNang;
+
+    public ServiceFunction() {
+
+    }
+
+    public ServiceFunction(String MaChucNang, String TenChucNang) {
+        this.MaChucNang = MaChucNang;
+        this.TenChucNang = TenChucNang;
+    }
+}
