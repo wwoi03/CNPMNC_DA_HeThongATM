64c830b900b0f26ceefe35f3d065946fe41df637
Aben1002 <92834190+ThanhTung2003@users.noreply.github.com>
2023-10-06 08:13:01 +0700

    update 1

--- Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Controllers/TransferMoneyController.cs
+++ Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Controllers/TransferMoneyController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web_CNPMNC_DA_HeThongATM.Models;
 
 namespace Web_CNPMNC_DA_HeThongATM.Controllers
 {
     public class TransferMoneyController : Controller
     {
+        FirebaseHelper firebaseHelper = new FirebaseHelper();
         public IActionResult Index()
         {
+
             return View();
+        }
+        [HttpPost]
+        public IActionResult ChuyenTien(AccountViewModel account)
+        {
+            double sotien = account.SoTien;
+            firebaseHelper.RutTien(sotien, account.SoTaiKhoan);
+            return RedirectToAction("Index");
         }
     }
 }
