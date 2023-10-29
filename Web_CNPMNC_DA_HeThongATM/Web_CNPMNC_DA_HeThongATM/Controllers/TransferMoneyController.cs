f4a33a04f16aa50936cee0316dce6d1d1ecadebe
Aben1002 <92834190+ThanhTung2003@users.noreply.github.com>
2023-10-29 14:23:03 +0700

    sửa form

--- Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Controllers/TransferMoneyController.cs
+++ Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Controllers/TransferMoneyController.cs
@@ -18,5 +18,43 @@ namespace Web_CNPMNC_DA_HeThongATM.Controllers
             firebaseHelper.RutTien(sotien, account.SoTaiKhoan);
             return RedirectToAction("Index");
         }
+        //public Account(Account firebaseClient)
+        //{
+        //    firebaseClient = firebaseClient;
+        //}
+
+        //[HttpGet]
+        //public ActionResult<string> GetAccount(string value)
+        //{
+        //    string info = firebaseHelper.GetAccount(value);
+        //    if (info != null)
+        //    {
+        //        return Ok(info); // Trả về thông tin tài khoản nếu tìm thấy
+        //    }
+        //    else
+        //    {
+        //        return NotFound("Không tìm thấy tài khoản"); // Trả về lỗi nếu không tìm thấy tài khoản
+        //    }
+        //}
+
+        //[HttpPost]
+        //public ActionResult<string> RutTien([FromBody] NapTienRequestModel model)
+        //{
+        //    if (model == null || string.IsNullOrEmpty(model.SoTaiKhoan) || model.SoTien <= 0)
+        //    {
+        //        return BadRequest("Dữ liệu không hợp lệ");
+        //    }
+
+        //    bool RutTienResult = firebaseHelper.NapTien(model.SoTien, model.SoTaiKhoan);
+
+        //    if (RutTienResult)
+        //    {
+        //        return Ok("Rút tiền thành công");
+        //    }
+        //    else
+        //    {
+        //        return BadRequest("Rút tiền thất bại"); // Trả về lỗi nếu Rút tiền thất bại
+        //    }
+        //}
     }
 }
