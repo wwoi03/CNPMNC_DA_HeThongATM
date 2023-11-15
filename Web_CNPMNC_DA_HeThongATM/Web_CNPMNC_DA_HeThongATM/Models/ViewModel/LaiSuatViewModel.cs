a0b234d73d7f5fed2398435c0afee92d6d0a4b92
Aben1002 <92834190+ThanhTung2003@users.noreply.github.com>
2023-11-15 18:33:44 +0700

    update 99.1%

--- Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Models/ViewModel/LaiSuatViewModel.cs
+++ Web_CNPMNC_DA_HeThongATM/Web_CNPMNC_DA_HeThongATM/Models/ViewModel/LaiSuatViewModel.cs
@@ -1,10 +1,16 @@
-﻿namespace Web_CNPMNC_DA_HeThongATM.Models.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_CNPMNC_DA_HeThongATM.Models.ViewModel
 {
     public class LaiSuatViewModel
     {
+        [Required(ErrorMessage = "Mã lãi suất là trường bắt buộc.")]
         public string Key { get; set; }
+
+        [Required(ErrorMessage = "Kỳ hạn là trường bắt buộc.")]
         public string KyHan { get; set; }
+
+        [Required(ErrorMessage = "Tỉ lệ là trường bắt buộc.")]
         public double TiLe { get; set; }
     }
 }
-
