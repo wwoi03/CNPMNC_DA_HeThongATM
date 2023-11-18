4e097884334736960604fd219234f97cbe33d636
yen15 <lethihaiyen26012003@gmail.com>
2023-11-18 21:22:44 +0700

    xuii

--- App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/Model/DatLichHen.java
+++ App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/Model/DatLichHen.java
@@ -24,6 +24,7 @@ public class DatLichHen {
         MaNhanVienKey = maNhanVienKey;
         NgayDenHen = ngayDenHen;
         TrangThai = trangThai;
+        //moi
     }
 
     public String getKey() {
