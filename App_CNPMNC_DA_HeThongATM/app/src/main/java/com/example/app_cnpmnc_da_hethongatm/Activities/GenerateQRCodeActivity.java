7e3202c7f9af3c293ea844a714d7d17227067e57
wwoi03 <tuandao8826848@gmail.com>
2023-11-19 20:46:49 +0700

    merge code Nam "Update tạo QR"

--- App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/Activities/GenerateQRCodeActivity.java
+++ App_CNPMNC_DA_HeThongATM/app/src/main/java/com/example/app_cnpmnc_da_hethongatm/Activities/GenerateQRCodeActivity.java
@@ -2,6 +2,8 @@ package com.example.app_cnpmnc_da_hethongatm.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -10,8 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.example.app_cnpmnc_da_hethongatm.Adapter.ListAccountQRCodeAdapter;
+import com.example.app_cnpmnc_da_hethongatm.Adapter.ListBeneficiaryAdapter;
 import com.example.app_cnpmnc_da_hethongatm.Extend.Config;
+import com.example.app_cnpmnc_da_hethongatm.Model.TaiKhoanLienKet;
+import com.example.app_cnpmnc_da_hethongatm.Model.ThuHuong;
 import com.example.app_cnpmnc_da_hethongatm.R;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,10 +29,14 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
 public class GenerateQRCodeActivity extends AppCompatActivity {
     private Config config;
-    EditText edit_amount, edit_message;
+    EditText edit_amount, edit_message, edit_tkqr;
+
+    ImageView img_select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,46 +48,69 @@ public class GenerateQRCodeActivity extends AppCompatActivity {
         edit_amount.setCursorVisible(false);
         edit_message = findViewById(R.id.edit_message);
         edit_message.setCursorVisible(false);
+        edit_tkqr = findViewById(R.id.edit_list);
+        img_select = findViewById(R.id.img_select);
         config = new Config(this);
+
+        img_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseRecyclerOptions<TaiKhoanLienKet> options =
+                        new FirebaseRecyclerOptions.Builder<TaiKhoanLienKet>()
+                                .setQuery(FirebaseDatabase.getInstance().getReference().child("TaiKhoanLienKet").orderByChild("MaKHKey").equalTo(config.getCustomerKey()), TaiKhoanLienKet.class)
+                                .build();
+
+                ListAccountQRCodeAdapter listAccountQRCodeAdapter = new ListAccountQRCodeAdapter(options);
+
+                DialogPlus dialogPlus = DialogPlus.newDialog(GenerateQRCodeActivity.this)
+                        .setContentHolder(new ViewHolder(R.layout.activity_list_accountqr))
+                        .setExpanded(true, 800)
+                        .setOverlayBackgroundResource(android.R.color.transparent) // Đặt màu nền trong suốt
+                        .create();
+
+                // Tìm RecyclerView trong layout của DialogPlus
+                RecyclerView recyclerView = dialogPlus.getHolderView().findViewById(R.id.rc_listaccountqr);
+
+                // Thiết lập ListAccountQRCodeAdapter cho RecyclerView
+                recyclerView.setLayoutManager(new LinearLayoutManager(GenerateQRCodeActivity.this));
+                recyclerView.setAdapter(listAccountQRCodeAdapter);
+
+                listAccountQRCodeAdapter.startListening();
+
+                // Thiết lập sự kiện click cho mỗi item
+                listAccountQRCodeAdapter.setOnItemClickListener(new ListAccountQRCodeAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(TaiKhoanLienKet model) {
+                        long soTaiKhoan = model.getSoTaiKhoan();
+                        String stkstring = Long.toString(soTaiKhoan);
+                        edit_tkqr.setText(stkstring);
+                        dialogPlus.dismiss();// đóng dialogplus
+                    }
+                });
+
+                dialogPlus.show();
+            }
+        });
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseReference taiKhoanLienKet = FirebaseDatabase.getInstance().getReference().child("TaiKhoanLienKet");
-                taiKhoanLienKet.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                            String maKhKey = dataSnapshot.child("MaKHKey").getValue(String.class);
-                            String customerKey = config.getCustomerKey();
-                            if (maKhKey.equals(customerKey)){
-                                Long SoTaiKhoan = dataSnapshot.child("SoTaiKhoan").getValue(Long.class);
-                                String TenKH = dataSnapshot.child("TenTK").getValue(String.class);
-                                EditText editAmount = findViewById(R.id.edit_amount);
-                                String amountString = editAmount.getText().toString();
-                                long amount = amountString.isEmpty() ? 0 : Long.parseLong(amountString);
-                                String tien = String.valueOf(amount);
-                                String message = edit_message.getText().toString();
-                                String qrCodeData = SoTaiKhoan + "," + TenKH + "," + tien + "," + message;
-                                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
-                                try {
-                                    BitMatrix bitMatrix = multiFormatWriter.encode(qrCodeData, BarcodeFormat.QR_CODE, 200, 200);
-                                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
-                                    imageView.setImageBitmap(bitmap);
-                                } catch (WriterException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
+                String SoTaiKhoan = edit_tkqr.getText().toString();
+                String amountString = edit_amount.getText().toString();
+                long amount = amountString.isEmpty() ? 0 : Long.parseLong(amountString);
+                String tien = String.valueOf(amount);
+                String message = edit_message.getText().toString();
+                String qrCodeData = SoTaiKhoan + "," + tien + "," + message;
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(qrCodeData, BarcodeFormat.QR_CODE, 200, 200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    imageView.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
